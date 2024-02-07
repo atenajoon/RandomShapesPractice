491630424e5599ecc5c0283ba6c52e59488a633b
Atena Razm <atenarazm@gmail.com>
2024-02-07 14:38:13 -0800

    unique config is picked

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ const wrapper = document.getElementById("wrapper");
 
 const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
+let prev = 0;
+const uniqueRand = (min, max, prev) => {
+  let next = prev;
+
+  while (prev === next)
+    next = rand(min, max);
+  return next;
+
+}
+
 // 3 configs
 const config = [1, 2, 3];
 
 setInterval(() => {
-  const index = rand(0, config.length);
+  const index = uniqueRand(0, config.length, prev);
 
   wrapper.dataset.configuration = config[index];
   console.log(index);
+
+  prev = index;
 }, 1000);
