8187276e65ea7f18979ccfed696e5ad105b75a93
Atena Razm <atenarazm@gmail.com>
2024-02-08 23:18:29 -0800

    all shapes and roundness configs are rendered smoothly2

--- script.js
+++ script.js
@@ -3,25 +3,31 @@ const wrapper = document.getElementById("wrapper");
 const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 let prev = 0;
+
 const uniqueRand = (min, max, prev) => {
   let next = prev;
 
   while (prev === next)
     next = rand(min, max);
-  return next;
 
+  return next;
 }
 
 // 3 configs
-const config = [1, 2, 3];
+const configCombinations = [
+  { configuration: 1, roundness: 1 },
+  { configuration: 2, roundness: 2 },
+  { configuration: 3, roundness: 3 }
+];
 
 setInterval(() => {
-  const index = uniqueRand(0, config.length, prev);
+  const index = uniqueRand(0, configCombinations.length, prev);
+  const combination = configCombinations[index];
 
-  wrapper.dataset.configuration = config[index];
-  console.log(index);
+  wrapper.dataset.configuration = combination.configuration;
+  wrapper.dataset.roundness = combination.roundness;
 
   prev = index;
 }, 3000);
 
-// wrapper.dataset.configuration = 1;
+// wrapper.dataset.configuration = 2;
